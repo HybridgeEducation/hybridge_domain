a465ef48ab0bbd37490fc8715059a432c643b420
Gerardo Mathus <gerardo@nextia.mx>
2022-04-27 15:17:52 -0500

    add movements

--- lib/entities/student_account.rb
+++ lib/entities/student_account.rb
@@ -3,6 +3,7 @@ module HybridgeDomain
     module Entity
         class StudentAccount < BaseEntity
             belongs_to :student
+            has_many :movements, class_name: "AccountMovement"
         end          
     end
 end
