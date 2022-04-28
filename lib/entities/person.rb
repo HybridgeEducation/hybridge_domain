a392cfe89da0c31b17a8c607b6af7cede993d623
Gerardo Mathus <gerardo@nextia.mx>
2022-04-28 10:06:51 -0500

    add person student rel

--- lib/entities/person.rb
+++ lib/entities/person.rb
@@ -2,6 +2,7 @@ require 'entities/base_entity'
 module HybridgeDomain
     module Entity
         class Person < BaseEntity
+            has_one :student
             def to_s
                 full_name
             end
